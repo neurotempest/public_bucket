f4a20e7c62c762b03a0beec51eac0f12344d6c0c
neurotempest <jdc.tas@gmail.com>
2021-10-14 17:56:12 +0300

    cryptography: Add example signing message with go-ethereum

--- cryptography/message_signing_test.go
+++ cryptography/message_signing_test.go
@@ -1,14 +1,19 @@
 package cryptography_test
 
 import (
+	"crypto/ecdsa"
+	//"crypto/elliptic"
 	"fmt"
 	"encoding/base64"
 	"testing"
+	"strings"
 
 	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/stretchr/testify/require"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
 func TestSignAndVerify_BCH(t *testing.T) {
@@ -49,3 +54,52 @@ func TestSignAndVerify_BCH(t *testing.T) {
 	require.True(t, address==recoveredAddress, "Message verification failed")
 }
 
+
+func TestSignAndVerify_ETH_using_go_ethereum(t *testing.T) {
+	privKey, err := ecdsa.GenerateKey(
+		secp256k1.S256(),
+		strings.NewReader("some random string of chars... this is probably a weak private key seed"),
+	)
+	require.NoError(t, err)
+	fmt.Printf("PrivKey: %x\n", crypto.FromECDSA(privKey))
+
+	pubKey := privKey.Public()
+	pubKeyECDSA, ok := pubKey.(*ecdsa.PublicKey)
+	require.True(t, ok)
+	fmt.Printf("PubKey: %x\n", crypto.FromECDSAPub(pubKeyECDSA))
+
+	address := crypto.PubkeyToAddress(*pubKeyECDSA)
+	fmt.Printf("Address: %s\n", address.Hex())
+
+	messageToSign := "some_message_to_sign"
+
+	signature, err := crypto.Sign(
+		crypto.Keccak256([]byte(messageToSign)),
+		privKey,
+	)
+	require.NoError(t, err)
+	fmt.Printf("Signature: %x\n", signature)
+
+	// crypto.VerifySignature takes the signature as [[ R || S ]] whereas crypto.Sign
+	// returns the signture as [[ R || S || V ]] (where V is either 0 or 1) so we remove
+	// V here to use with VerifySignature
+	sigWithoutVSuffix := signature[:len(signature)-1]
+	verified := crypto.VerifySignature(
+		crypto.FromECDSAPub(pubKeyECDSA),
+		crypto.Keccak256([]byte(messageToSign)),
+		sigWithoutVSuffix,
+	)
+	require.True(t, verified, "Message verification failed (using crypto.VerifySignature)")
+
+
+	recoveredPubKey, err := crypto.SigToPub(
+		crypto.Keccak256([]byte(messageToSign)),
+		signature,
+	)
+	require.NoError(t, err)
+
+	recoveredAddress := crypto.PubkeyToAddress(*recoveredPubKey)
+	fmt.Printf("recovered Address: %s\n", recoveredAddress.Hex())
+
+	require.True(t, address==recoveredAddress, "Message verification failed (using crypto.SigToPub)")
+}
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.16
 require (
 	github.com/btcsuite/btcd v0.22.0-beta
 	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce
+	github.com/ethereum/go-ethereum v1.10.9
 	github.com/stretchr/testify v1.7.0
 )
